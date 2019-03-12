68a98ff0b6fa8ebbc37beb4bc57a91695d5cd485
eyjhbb@gmail.com <eyjhbb@gmail.com>
2019-03-12 18:17:04 +0100

    only replace ' -- ' with two new lines

--- multiline-dash.py
+++ multiline-dash.py
@@ -2,7 +2,7 @@ SCRIPT_NAME    = "multiline-dash"
 SCRIPT_AUTHOR  = "eyJhb <eyjhbb@gmail.com>"
 SCRIPT_VERSION = "0.1"
 SCRIPT_LICENSE = "GPLv3"
-SCRIPT_DESC    = "Replaces dashdash (--) with a two newlines instead symbol"
+SCRIPT_DESC    = "Replaces dashdash ` -- ` with a two newlines instead symbol"
 
 import_ok = True
 
@@ -13,8 +13,7 @@ except:
    import_ok = False
 
 def convert_dash_to_newline(data, modifier, modifier_data, string):
-    string = string.replace(" -- ", "--")
-    return string.replace("--", "\n\n")
+    return string.replace(" -- ", "\n\n")
 
 if __name__ == "__main__" and import_ok:
     if w.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE,
